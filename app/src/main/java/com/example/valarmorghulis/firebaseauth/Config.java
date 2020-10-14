8e8c1fd229f24acebe446bc5bffd00c1302929c7
CAravindhan <aravindhanofficials@gmail.com>
2020-10-14 20:10:14 +0530

    MAD Mini-Project Sell IT

---
+++ app/src/main/java/com/example/valarmorghulis/firebaseauth/Config.java
@@ -0,0 +1,6 @@
+package com.example.valarmorghulis.firebaseauth;
+
+public class Config {
+    public static final String EMAIL = "bookbuzz121@tutanota.com";
+    public static final String PASSWORD = "@121BookBuzz";
+}
